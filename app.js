6392323704f66f7e775a0761aa2fed5013b72e68
Kittichai Raksawong <93074696+dadaKitt@users.noreply.github.com>
2023-03-19 00:35:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const { Client, Events, GatewayIntentBits, SlashCommandBuilder} = require('discord.js');
 const { token } = require('./config.json');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { chat } = require('./src/chat.js');
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
+
+
+
+
+
 
 client.once(Events.ClientReady, c => {
 
@@ -8,10 +21,14 @@ client.once(Events.ClientReady, c => {
     console.log('-----------------------------');
     console.log(currentDate);
     console.log('Yeji Version DEV');
-    console.log('Develop by : HONIX');
+    console.log('Develop by : HONIX Academy');
+    console.log('Develop by : RIN');
+    console.log('Develop by : Kim');
     console.log('-----------------------------');
     
 
 });
+
+
 
 client.login(token);
